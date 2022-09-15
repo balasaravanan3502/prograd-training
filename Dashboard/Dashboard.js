f2f27656fca8d15055875a85446189b4a1622805
balasaravanan3502 <balasaravananvp35@gmail.com>
2022-09-15 23:25:59 +0530

    Added. Mobile navbar

---
+++ Dashboard/Dashboard.js
@@ -0,0 +1,21 @@
+const openListener = document.querySelector(".nav-hambuger");
+
+openListener.addEventListener("click", openToogle);
+
+const closeListener = document.querySelector(".sidenav-hambuger");
+
+closeListener.addEventListener("click", closeToggle);
+
+function openToogle() {
+  const sideNavBartoggler = document.querySelector(".sidebar");
+  document.querySelector(".sidebar").style.justifyContent = "space-evenly";
+  document.querySelector(".navbar-user-profile").style.display = "block";
+  sideNavBartoggler.classList.add("nav-hambuger-open");
+}
+
+function closeToggle() {
+  const sideNavBartoggler = document.querySelector(".sidebar");
+  document.querySelector(".sidebar").style.justifyContent = "space-evenly";
+  document.querySelector(".navbar-user-profile").style.display = "none";
+  sideNavBartoggler.classList.remove("nav-hambuger-open");
+}
